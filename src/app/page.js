789b88e83dcee05ec9f07d5bbddd634f6b7b7338
Joran Hennion <joran.hennion@gmail.com>
2024-10-11 20:02:02 +0200

    last build and fix sample page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
+import { Kranky } from 'next/font/google'
 
-export default function Home() {
+export default function main() {
   return (
-    {/* jsx here */ }
+    /* sample jsx, delete this to the comment after this to replace with your own */
+    <div id="example" className="w-[100vw] h-[100vh] flex justify-center justify-items-center bg-black text-white">
+      <p className="font-extrabold m-auto text-6xl hover:text-7xl">example</p>
+    </div>
+    /* delete/replace till here */
   );
 }
